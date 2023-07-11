c0e575918479151a0e299e6df47332b939a4ea2e
akhileshkumar23 <132866952+akhileshkumar23@users.noreply.github.com>
2023-07-11 18:30:59 +0530

    Add files via upload

---
+++ school/school/src/main/java/com/abc/school/BhsApplication.java
@@ -0,0 +1,13 @@
+package com.abc.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhsApplication.class, args);
+	}
+
+}
